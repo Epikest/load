9f97faca30664e798330620b39bce73b54450e61
Epik <epik.est@pm.me>
2022-06-29 18:04:57 +0000

    Update store.lua

--- store.lua
+++ store.lua
@@ -2,3 +2,4 @@ if (getgenv().dev ~= true) then
 	loadstring(game:HttpGetAsync("https://cdn.jsdelivr.net/gh/Infinite-Store/Infinite-Store@development/main.lua"))
 else
 	loadstring(game:HttpGetAsync("https://cdn.jsdelivr.net/gh/Infinite-Store/Infinite-Store@main/main.lua"))
+end
